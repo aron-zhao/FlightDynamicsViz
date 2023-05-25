e90de15189467d2633f798722f12d33a94a792fa
Aron Zhao <aronzhao@outlook.com>
2023-05-25 15:26:36 -0400

    GH pages testing

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
-import * as THREE from './three.js';
+import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
